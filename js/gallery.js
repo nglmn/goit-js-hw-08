1741e9371cb7c46ab87c0de0cf61783ee3605094
Artem Liskovshchenko <artem140742@gmail.com>
2024-01-08 16:11:17 +0200

    asdasdas

--- js/gallery.js
+++ js/gallery.js
@@ -65,50 +65,51 @@ const images = [
     },
 ];
 
-const gallery = document.querySelector('.gallery');
+const imagesGallery = document.querySelector('.gallery');
 
 const markup = images
     .map(({ preview, original, description }) => {
-        return `<li class="gallery-item">
-                    <a class="gallery-link" href="${original}">
-                        <img
-                            class="gallery-image"
-                            src="${preview}"
-                            data-source="${original}"
-                            alt="${description}"
-                        />
-                    </a>
-                </li>`
+        return `
+        <li class="gallery-item">
+            <a class="gallery-link" href="${original}">
+                <img
+                    class="gallery-image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+    `;
     })
     .join('');
 
-gallery.innerHTML = markup;
+imagesGallery.insertAdjacentHTML('afterbegin', markup);
 
-let instance;
-function openImage(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
+imagesGallery.addEventListener('click', handleClickOpenImage);
+
+function handleClickOpenImage(e) {
+    e.preventDefault();
+    const selectImage = e.target.parentNode.getAttribute('href');
+
+    if (e.target.nodeName !== 'IMG') {
         return;
-    }
-    const selectedItem = event.target.parentNode.getAttribute('href');
-
-    instance = basicLightbox.create(`
-        <img src="${selectedItem}" width="800" height="600">
-    `, {
-        onShow: (instance) => console.log('onShow', instance),
-        onClose: (instance) => console.log('onClose', instance)
-    })
-
-    instance.show();
-
-    document.addEventListener('keyup', closeImage);
-}
-function closeImage(event) {
-    if (event.key === 'Escape') {
-        instance.close();
+    } else {
+        createLightbox(selectImage);
     }
 }
-
-gallery.addEventListener('click', openImage);
+function createLightbox(img) {
+    const instance = basicLightbox.create(`
+        <img src="${img}" width="800" height="600">
+    `);
+    const visible = instance.show();
+    if (visible) {
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                instance.close();
+            }
+        })
+    }
+}
 
 
